4b8ab0f9ffde87862d2fc5714393bc5b5bdfb592
Gabaranth <emmanuel-v62@hotmail.fr>
2022-11-03 12:01:55 +0100

    Tuto vérification d'email terminer avec JWT

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Users;
 use App\Form\RegistrationFormType;
+use App\Repository\UsersRepository;
 use App\Security\UsersAuthenticator;
 use App\Service\JWTService;
 use App\Service\SendMailService;
@@ -34,7 +35,7 @@ class RegistrationController extends AbstractController
                 )
             );
 
-            //$entityManager->persist($user);
+            $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
 
@@ -71,12 +72,59 @@ class RegistrationController extends AbstractController
     }
 
     #[Route('/vertif/{token}', name: 'verify_user')]
-    public function verifyUser($token, JWTService $jwt): Response 
+    public function verifyUser($token, JWTService $jwt, EntityManagerInterface $em,  UsersRepository $usersRepository ): Response 
     {
         if ($jwt->isValid($token) && !$jwt->IsExpired($token) && $jwt->check($token, $this->getParameter('app.jwtsecret'))) {
-            
+            $payload = $jwt->getPayload($token);
+            $user = $usersRepository->find($payload['user_id']);
+
+            if ($user && !$user->getIsVerified()) {
+                $user->setIsVerified(true);
+                $em->flush($user);
+                $this->addFlash('success', 'Utilisateur activé');
+                return $this->redirectToRoute('profile_index');
+            }
         }
         $this->addFlash('danger', 'Le token est invalide ou a expiré');
         return $this->redirectToRoute('app_login');
+    }
+
+    #[Route('/renvoiverif', name: 'resend_verif')]
+    public function resendVerif(JWTService $jwt, SendMailService $mail, UsersRepository $usersRepository): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            $this->addFlash('danger', 'Vous devez être connecté pour accéder à cette page');
+            return $this->redirectToRoute('app_login');
+        }
+
+        if ($user->getIsVerified()) {
+            $this->addFlash('warning', 'Cet utilisateur est déjà activé');
+            return $this->redirectToRoute('profile_index');
+        }
+
+        $header = [
+            'typ' => 'JWT',
+            'alg' => 'HS256'
+        ];
+
+        $payload = [
+            'user_id' => $user->getId()
+        ];
+
+        $token = $jwt->generate($header, $payload, $this->getParameter('app.jwtsecret'));
+
+
+        $mail->send(
+            'no-reply@monsite.net',
+            $user->getEmail(),
+            'Activation de votre compte sur le site e-commerce',
+            'register',
+            compact('user', 'token')
+        );
+
+        $this->addFlash('success', 'Email de vérification envoyé');
+        return $this->redirectToRoute('profile_index');
     }
 }
