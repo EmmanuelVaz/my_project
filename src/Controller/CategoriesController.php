651dcdd6563933b8d443dc419353e7e839a2638c
Gabaranth <emmanuel-v62@hotmail.fr>
2022-11-09 09:11:05 +0100

    Mise en place d'une pagination et création de la page détails des produits

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Categories;
+use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +13,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategoriesController extends AbstractController
 {
     #[Route('/{slug}', name: 'list')]
-    public function list(Categories $category): Response
+    public function list(Categories $category, ProductsRepository $productsRepository, Request $request): Response
     {
-        $products = $category->getProducts();
+        $page = $request->query->getInt('page', 1);
 
+        $products = $productsRepository->findProductsPaginated($page, $category->getSlug(), 2);
+        
         return $this->render('categories/list.html.twig', compact('category', 'products'));
     }
 }
--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,7 +2,10 @@
 namespace App\Controller;
 
 use App\Entity\Products;
+use App\Form\AddProductType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,6 +22,25 @@ class ProductsController extends AbstractController
     public function details(Products $product): Response
     {
         return $this->render('products/details.html.twig', compact('product'));
+    }
+
+
+
+    #[Route('/ajout_produit', name: 'add_products')]
+    public function add_products(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $product = new Products();
+        $form = $this->createForm(AddProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($product);
+            $entityManager->flush();
+        }
+
+        return $this->render('products/add.html.twig', [
+            'addProductsForm' => $form->createView(),
+        ]);
     }
 }
 ?>
