7a570a34ca408c747b8a11d22f581c023bd74115
Gabaranth <emmanuel-v62@hotmail.fr>
2022-11-22 12:59:48 +0100

    Suite tuto "Mise en place des permissions utilisateurs"

--- migrations/Version20221122100113.php
+++ migrations/Version20221122100113.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221103120550 extends AbstractMigration
+final class Version20221122100113 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20221103120550 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE users ADD reset_token VARCHAR(100) NOT NULL');
+        $this->addSql('ALTER TABLE categories ADD category_order INT NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE users DROP reset_token');
+        $this->addSql('ALTER TABLE categories DROP category_order');
     }
 }
--- src/DataFixtures/CategoriesFixtures.php
+++ src/DataFixtures/CategoriesFixtures.php
@@ -14,27 +14,28 @@ class CategoriesFixtures extends Fixture
     public function __construct(private SluggerInterface $slugger){}
     public function load(ObjectManager $manager): void
     {
-        $parent = $this->createCategory('Informatique', null, $manager);
+        $parent = $this->createCategory('Informatique', null, 1, $manager);
 
-        $this->createCategory('Ordinateurs portable', $parent, $manager);
-        $this->createCategory('Ecran', $parent, $manager);
-        $this->createCategory('Souris', $parent, $manager);
+        $this->createCategory('Ordinateurs portable', $parent, 2, $manager);
+        $this->createCategory('Ecran', $parent, 2, $manager);
+        $this->createCategory('Souris', $parent, 3, $manager);
 
-        $parent = $this->createCategory('Mode', null, $manager);
+        $parent = $this->createCategory('Mode', null, 4, $manager);
 
-        $this->createCategory('Homme', $parent, $manager);
-        $this->createCategory('Femme', $parent, $manager);
-        $this->createCategory('Enfant', $parent, $manager);
+        $this->createCategory('Homme', $parent, 5, $manager);
+        $this->createCategory('Femme', $parent, 6, $manager);
+        $this->createCategory('Enfant', $parent, 6, $manager);
 
         $manager->flush();
     }
 
-    public function createCategory(string $name, Categories $parent = null, ObjectManager $manager){
+    public function createCategory(string $name, Categories $parent = null, int $categoryOrder ,ObjectManager $manager){
         
         $category = new Categories();
         $category->setName($name);
         $category->setSlug($this->slugger->slug($category->getName())->lower());
         $category->setParent($parent);
+        $category->setcategoryOrder($categoryOrder);
         $manager->persist($category);
 
         $this->addReference('cat-'.$this->counter, $category);
